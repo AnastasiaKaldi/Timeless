0166b2c81e6695d127f77da0ab1a29b579d020dd
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-10-11 16:30:54 +0100

    Deployment Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-// import HomePage from "../src/HomePage.jsx";
+import HomePage from "../src/HomePage.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      {/* <HomePage /> */}
-
-      <div className="bg-black">
-        <h1 className="text-red-500" style={{ fontFamily: "Pixeloid" }}>
-          Are you ready to time travel?
-        </h1>
-      </div>
-    </>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black">
-      <h1 className="text-white" style={{ fontFamily: "Pixeloid" }}>
-        Vite + React
+    <div className="bg-amber-400 w-full h-full">
+      <h1 className="" style={{ fontFamily: "Pixeloid" }}>
+        Are you ready to time travel???
       </h1>
     </div>
   );
